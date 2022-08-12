b3af9dfc656cd605a5e36515106a32e85f0a4a2e
kennedy <kennedyngosachanda@gmail.com>
2022-08-12 15:13:20 +0200

    intial clean up

--- App/api/app.js
+++ App/api/app.js
@@ -1,15 +1,29 @@
-
+/*
+        THIS IS A FINAL YEAR BACKEND APPLICATION CODE IN JAVASCRIPT, PRISMA, MYSQL
+        AND EXPRESS.
+*/
 const express = require('express')
+const cors = require("cors");
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
 
 
-const passengerRouter = require("./routes/passenger");
-const operatorRouter = require("./routes/operator");
+const passengerAuthRouter = require("./routes/passengerAuth");
+const operatorAuthRouter = require("./routes/operatorAuth");
 
 
-app.use('/passenger', passengerRouter);
-app.use('/operator', operatorRouter);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors());
+
+
+app.use('/auth/passenger', passengerAuthRouter);
+app.use('/auth/operator', operatorAuthRouter);
 
 app.listen( process.env.port ||port, () => {
   console.log(`Modern E bus system running at port ${port}`)
