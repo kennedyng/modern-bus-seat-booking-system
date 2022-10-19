a2ad447773329b795b2c5c885c4d33325f58acb8
kennedy <kennedyngosachanda@gmail.com>
2022-10-19 17:31:10 +0200

    Update query.js

--- api/controllers/trip/query.js
+++ api/controllers/trip/query.js
@@ -1,74 +1,71 @@
 const operatorTripsQuery = {
-    totalQuery: ( req ) => {
-        return {
-            where: {
-                operatorId: Number(req.userData.operatorId)
-            },
-            select: {
-                id: true,
-            }
-            
-        }
-    },
-    dataQuery: ( req ) => {
-        return {
-            where: {
-                operatorId: Number(req.userData.operatorId)
-            },
-            orderBy: {
-                departing_time: "desc"
-
-            },
-            include: {
-                Route: true,
-                Bus: true
-            },
-        }
-    }, 
-    intialDataSize: 5,
-    model: "trip"
-}
+  totalQuery: (req) => {
+    return {
+      where: {
+        operatorId: Number(req.userData.operatorId),
+      },
+      select: {
+        id: true,
+      },
+    };
+  },
+  dataQuery: (req) => {
+    return {
+      where: {
+        operatorId: Number(req.userData.operatorId),
+      },
+      orderBy: {
+        departing_time: "desc",
+      },
+      include: {
+        Route: true,
+        Bus: true,
+        receipt: true,
+      },
+    };
+  },
+  intialDataSize: 5,
+  model: "trip",
+};
 const tripsQuery = {
-    totalQuery: ( req ) => {
-        return {
-            where: {
-                Route: {
-                    AND: [
-                        { ending_point: req.params.ending_point },
-                        { starting_point: req.params.starting_point },
-                    ]
-                },
-                operatorId: Number( req.params.operatorId )
-            }
-        }
-    },
-    dataQuery: ( req ) => {
-        return {
-            where: {
-                Route: {
-                    
-                    AND: [
-                        { ending_point: req.params.ending_point },
-                        { starting_point: req.params.starting_point },
-                    ],
-                },
-                operatorId: Number( req.params.operatorId )
-            },
-            orderBy: {
-                departing_time: "desc"
-
-            },
-            include: {
-                Route: true,
-                Bus: true
-            },
-        }
-    }, 
-    intialDataSize: 5,
-    model: "trip"
-}
+  totalQuery: (req) => {
+    return {
+      where: {
+        Route: {
+          AND: [
+            { ending_point: req.params.ending_point },
+            { starting_point: req.params.starting_point },
+          ],
+        },
+        operatorId: Number(req.params.operatorId),
+      },
+    };
+  },
+  dataQuery: (req) => {
+    return {
+      where: {
+        Route: {
+          AND: [
+            { ending_point: req.params.ending_point },
+            { starting_point: req.params.starting_point },
+          ],
+        },
+        operatorId: Number(req.params.operatorId),
+      },
+      orderBy: {
+        departing_time: "desc",
+      },
+      include: {
+        Route: true,
+        Bus: true,
+      },
+    };
+  },
+  intialDataSize: 5,
+  model: "trip",
+};
 
 module.exports = {
-    operatorTripsQuery,
-    tripsQuery
-}
+  operatorTripsQuery,
+  tripsQuery,
+};
