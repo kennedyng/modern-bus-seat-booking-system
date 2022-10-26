1331545b5f1109388137326e78a93db95ba1e639
kennedy <kennedyngosachanda@gmail.com>
2022-10-26 20:15:53 +0200

    Update auth.js

--- api/controllers/operator/auth.js
+++ api/controllers/operator/auth.js
@@ -1,8 +1,5 @@
-// const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
-// const prisma = new PrismaClient();
 
 const prisma = require("../../utils/prisma");
 const app = require("express")();
